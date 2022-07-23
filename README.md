fc3d923fb13f35122d858751c0f65f46cd490657
Saito Tsutomu <tsutomu7@hotmail.co.jp>
2022-07-23 18:59:10 +0900

    ini

--- README.md
+++ README.md
@@ -1,21 +1,20 @@
-## Blender Add-on: ShapeKeyBit
+## Blender Add-on: ShapeKeyPaste
 
 A little bit utility for shape keys.
 
-- Select Diff: Select different vertices from basis shape key and active shape key.
+- Sel Diff 2 Obj: Select the different vertices of 2 objects.
+- Select Diff from basis: Select the different vertices from basis and active shape key.
 - Set Vert: Set vertices from active shape key to target shape key.
-- Save CSV: Save the selected vertices of active shape key to the CSV file.
-- Load CSV: Load vertices from the CSV file.
 
 ## Installation
 
-- Download https://github.com/SaitoTsutomu/ShapeKeyBit/archive/refs/heads/main.zip
+- Download https://github.com/SaitoTsutomu/ShapeKeyPaste/archive/refs/heads/main.zip
 - Start Blender.
 - Edit menu -> Preferences
   - Select the "Add-ons" tab.
   - Press the "Install ...".
   - Select the downloaded ZIP file and press the button "Install Add-on".
-  - Check the "Object: ShapeKeyBit".
+  - Check the "Object: ShapeKeyPaste".
 
 ## Usage
 
--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ import bpy
 import numpy as np
 
 bl_info = {
-    "name": "ShapeKeyBit",
+    "name": "ShapeKeyPaste",
     "author": "tsutomu",
     "version": (0, 1),
     "blender": (3, 1, 0),
@@ -11,7 +11,7 @@ bl_info = {
     "description": "",
     "location": "View3D > Object",
     "warning": "",
-    "doc_url": "https://github.com/SaitoTsutomu/ShapeKeyBit",
+    "doc_url": "https://github.com/SaitoTsutomu/ShapeKeyPaste",
 }
 
 
@@ -23,12 +23,40 @@ def set_wireframe():
                     space.shading.type = "WIREFRAME"
 
 
+class CSK_OT_select_diff_objs(bpy.types.Operator):
+    """2つのオブジェクトの異なる点を選択"""
+
+    bl_idname = "object.select_diff_objs"
+    bl_label = "Sel Diff 2 Obj"
+    bl_description = "Select the different vertices of 2 objects."
+
+    def execute(self, context):
+        objs = [obj for obj in bpy.context.objects if obj.type == "MESH"]
+        if len(objs) != 2:
+            self.report({"INFO"}, "Select 2 objects.")
+            return {"CANCELLED"}
+        bpy.ops.object.mode_set(mode="EDIT")  # for deselect
+        bpy.ops.mesh.select_mode(type="VERT")
+        bpy.ops.mesh.select_all(action="DESELECT")
+        bpy.ops.object.mode_set(mode="OBJECT")  # for select
+        dif = set(tuple(vtx.co) for vtx in objs[0].data.vertices) ^ set(
+            tuple(vtx.co) for vtx in objs[1].data.vertices
+        )
+        for obj in objs:
+            for i, vtx in enumerate(obj.data.vertices):
+                if tuple(vtx.co) in dif:
+                    obj.data.vertices[i].select = True
+        bpy.ops.object.mode_set(mode="EDIT")  # for confirm
+        set_wireframe()
+        return {"FINISHED"}
+
+
 class CSK_OT_select_diff_from_basis(bpy.types.Operator):
     """シェイプキーのBasisの位置が異なる点を選択"""
 
     bl_idname = "object.select_diff_from_basis"
-    bl_label = "Select Diff"
-    bl_description = "Select different vertices from basis shape key and active shape key."
+    bl_label = "Select Diff from basis"
+    bl_description = "Select the different vertices from basis and active shape key."
 
     def execute(self, context):
         obj = context.object
@@ -54,144 +82,77 @@ class CSK_OT_select_diff_from_basis(bpy.types.Operator):
         return {"FINISHED"}
 
 
-class CSK_OT_set_vert_from_active(bpy.types.Operator):
-    """選択している点のシェイプキーの位置をターゲットに反映"""
+class CSK_OT_copy_vertices(bpy.types.Operator):
+    """選択している点のインデックスをコピー"""
 
-    bl_idname = "object.set_vert_from_active"
-    bl_label = "Set Vert"
-    bl_description = "Set vertices from active shape key to target shape key."
-
-    to_shape_key: bpy.props.IntProperty()  # type: ignore
+    bl_idname = "object.copy_vertices"
+    bl_label = "Copy Vert"
+    bl_description = "Copy the selected vertices from active shape key."
 
     def execute(self, context):
-        obj = context.object
-        if not obj or obj.type != "MESH":
-            self.report({"WARNING"}, "Select an object with shape key.")
+        if context.mode != "EDIT_MESH":
+            self.report({"WARNING"}, "Enter to edit mode.")
             return {"CANCELLED"}
-        from_key_id = obj.active_shape_key_index
-        bpy.ops.object.mode_set(mode="OBJECT")
+        obj = context.edit_object
+        s = " ".join(str(i) for i, v in enumerate(obj.data.vertices) if v.select)
+        context.window_manager.clipboard = f"ShapeKeyPaste {obj.active_shape_key_index} " + s
+
+
+class CSK_OT_paste_vertices(bpy.types.Operator):
+    """コピーされた点の位置をペースト"""
+
+    bl_idname = "object.paste_vertices"
+    bl_label = "Paste Vert"
+    bl_description = "Paste the position of vertices to active shape key."
+
+    def execute(self, context):
+        if context.mode != "EDIT_MESH":
+            self.report({"WARNING"}, "Enter to edit mode.")
+            return {"CANCELLED"}
+        obj = context.edit_object
+        ss = context.window_manager.clipboard.split()
+        if len(ss) < 3 or ss[0] != "ShapeKeyPaste" or obj.data.shape_keys:
+            self.report({"WARNING"}, "No data in the clipboard or no shape keys.")
+            return {"CANCELLED"}
         kb = obj.data.shape_keys.key_blocks
-        for i in range(len(obj.data.vertices)):
-            if obj.data.vertices[i].select:
-                kb[self.to_shape_key].data[i].co = kb[from_key_id].data[i].co
-        return {"FINISHED"}
-
-
-class CSK_OT_save_vert_to_csv(bpy.types.Operator):
-    """アクティブシェイプキーの選択した点の位置をCSVに出力"""
-
-    bl_idname = "object.save_vert_to_csv"
-    bl_label = "Save CSV"
-    bl_description = "Save the selected vertices of active shape key to the CSV file."
-
-    filepath: bpy.props.StringProperty()  # type: ignore # noqa
-
-    def invoke(self, context, event):
-        context.window_manager.fileselect_add(self)
-        return {"RUNNING_MODAL"}
-
-    def execute(self, context):
-        obj = context.object
-        if not obj or obj.type != "MESH":
-            self.report({"WARNING"}, "Select an object with shape key.")
-            return {"CANCELLED"}
-        key_id = obj.active_shape_key_index
-        bpy.ops.object.mode_set(mode="OBJECT")
-        with open(self.filepath, "w") as fp:
-            for i, skp in enumerate(obj.data.shape_keys.key_blocks[key_id].data):
-                if obj.data.vertices[i].select:
-                    fp.write(f"{key_id},{i},{skp.co.x:.8f},{skp.co.y:.8f},{skp.co.z:.8f}\n")
-        return {"FINISHED"}
-
-
-class CSK_OT_select_diff_objs(bpy.types.Operator):
-    """2つのオブジェクトの異なる点を選択"""
-
-    bl_idname = "object.select_diff_objs"
-    bl_label = "Sel Diff 2 Obj"
-    bl_description = "Select different vertices of 2 objects."
-
-    def execute(self, context):
-        objs = [obj for obj in bpy.context.selected_objects if obj.type == "MESH"]
-        if len(objs) != 2:
-            self.report({"INFO"}, "Select 2 objects.")
-            return {"CANCELLED"}
-        bpy.ops.object.mode_set(mode="EDIT")  # for deselect
-        bpy.ops.mesh.select_mode(type="VERT")
-        bpy.ops.mesh.select_all(action="DESELECT")
-        bpy.ops.object.mode_set(mode="OBJECT")  # for select
-        dif = set(tuple(vtx.co) for vtx in objs[0].data.vertices) ^ set(
-            tuple(vtx.co) for vtx in objs[1].data.vertices
-        )
-        for obj in objs:
-            for i, vtx in enumerate(obj.data.vertices):
-                if tuple(vtx.co) in dif:
-                    obj.data.vertices[i].select = True
-        bpy.ops.object.mode_set(mode="EDIT")  # for confirm
-        set_wireframe()
-        return {"FINISHED"}
-
-
-class CSK_OT_load_vert_from_csv(bpy.types.Operator):
-    """CSVからシェイプキーの点の位置を読込"""
-
-    bl_idname = "object.load_vert_from_csv"
-    bl_label = "Load CSV"
-    bl_description = "Load vertices from the CSV file."
-
-    filepath: bpy.props.StringProperty()  # type: ignore # noqa
-
-    def invoke(self, context, event):
-        context.window_manager.fileselect_add(self)
-        return {"RUNNING_MODAL"}
-
-    def execute(self, context):
-        obj = context.object
-        if not obj or obj.type != "MESH":
-            self.report({"WARNING"}, "Select an object with shape key.")
+        from_key_id = int(ss[1])
+        if not (0 <= from_key_id < len(kb)):
+            self.report({"WARNING"}, "Illegal key id.")
             return {"CANCELLED"}
         bpy.ops.object.mode_set(mode="OBJECT")
-        with open(self.filepath) as fp:
-            for s in fp:
-                key_id, idx, x, y, z = s.split(",")
-                key_id, idx = map(int, (key_id, idx))
-                x, y, z = map(float, (x, y, z))
-                obj.data.shape_keys.key_blocks[key_id].data[idx].co = x, y, z
+
+        to_key_id = obj.active_shape_key_index
+        for s in ss[2:]:
+            idx = int(s)
+            kb[to_key_id].data[idx].co = kb[from_key_id].data[idx].co
+        bpy.ops.object.mode_set(mode="EDIT")
         return {"FINISHED"}
 
 
 class CSK_PT_bit(bpy.types.Panel):
-    bl_label = "ShapeKeyBit"
+    bl_label = "ShapeKeyPaste"
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
     bl_category = "Edit"
 
     def draw(self, context):
-        text = CSK_OT_select_diff_from_basis.bl_label
-        self.layout.operator(CSK_OT_select_diff_from_basis.bl_idname, text=text)
-        self.layout.separator()
-        self.layout.prop(context.scene, "to_shape_key", text="Target ShapeKey Index")
-        prop = self.layout.operator(
-            CSK_OT_set_vert_from_active.bl_idname, text=CSK_OT_set_vert_from_active.bl_label
-        )
-        prop.to_shape_key = context.scene.to_shape_key
-        self.layout.separator()
-        text = CSK_OT_save_vert_to_csv.bl_label
-        self.layout.operator(CSK_OT_save_vert_to_csv.bl_idname, text=text)
-        self.layout.separator()
-        text = CSK_OT_load_vert_from_csv.bl_label
-        self.layout.operator(CSK_OT_load_vert_from_csv.bl_idname, text=text)
-        self.layout.separator()
         text = CSK_OT_select_diff_objs.bl_label
         self.layout.operator(CSK_OT_select_diff_objs.bl_idname, text=text)
+        text = CSK_OT_select_diff_from_basis.bl_label
+        self.layout.operator(CSK_OT_select_diff_from_basis.bl_idname, text=text)
+
+        self.layout.separator()
+        text = CSK_OT_copy_vertices.bl_label
+        self.layout.operator(CSK_OT_copy_vertices.bl_idname, text=text)
+        text = CSK_OT_paste_vertices.bl_label
+        self.layout.operator(CSK_OT_paste_vertices.bl_idname, text=text)
 
 
 ui_classes = (
-    CSK_OT_select_diff_from_basis,
-    CSK_OT_set_vert_from_active,
-    CSK_OT_save_vert_to_csv,
-    CSK_OT_load_vert_from_csv,
     CSK_OT_select_diff_objs,
+    CSK_OT_select_diff_from_basis,
+    CSK_OT_copy_vertices,
+    CSK_OT_paste_vertices,
     CSK_PT_bit,
 )
 
@@ -199,13 +160,11 @@ ui_classes = (
 def register():
     for ui_class in ui_classes:
         bpy.utils.register_class(ui_class)
-    bpy.types.Scene.to_shape_key = bpy.props.IntProperty(default=1)
 
 
 def unregister():
     for ui_class in ui_classes:
         bpy.utils.unregister_class(ui_class)
-    del bpy.types.Scene.to_shape_key
 
 
 if __name__ == "__main__":
