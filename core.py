8280ba5bbb40500fdc14706ed261e1d4cb7fa8ae
Saito Tsutomu <tsutomu7@hotmail.co.jp>
2022-09-04 10:32:14 +0900

    upd

--- core.py
+++ core.py
@@ -1,10 +1,34 @@
 import os
-from math import ceil, log2
+from dataclasses import dataclass
 
 import bpy
 from bpy.props import BoolProperty, IntProperty
 
 from .register_class import _get_cls
+
+
+@dataclass
+class ChangeImageSettings:
+    image: bpy.types.Image
+    quality: int
+
+    def __enter__(self):
+        n = self.image.depth // (self.image.is_float * 3 + 1) // 8
+        self.file_format = "JPEG" if n % 2 else "PNG"
+        self.color_mode = ["BW", "RGBA", "RGB", "RGBA"][n - 1]
+        self.__exit__(None, None, None)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        stng = bpy.context.scene.render.image_settings
+        self.quality, stng.quality = stng.quality, self.quality
+        # file_formatを変えるとcolor_modeが変わる可能性があるので、この順でないといけない
+        self.file_format, self.color_mode, stng.file_format, stng.color_mode = (
+            stng.file_format,
+            stng.color_mode,
+            self.file_format,
+            self.color_mode,
+        )
 
 
 class CSI_OT_slim_image(bpy.types.Operator):
@@ -18,21 +42,22 @@ class CSI_OT_slim_image(bpy.types.Operator):
     to_small: BoolProperty() = BoolProperty(default=True)  # type: ignore
 
     def execute(self, context):
+        bpy.context.scene.view_settings.view_transform = "Standard"
         os.makedirs("/tmp/img/", exist_ok=True)
-        bpy.context.scene.render.image_settings.quality = self.quality
-        bpy.context.scene.render.image_settings.file_format = "JPEG"
-
         bpy.ops.file.unpack_all(method="REMOVE")
         for img in bpy.data.images:
             if img.name == "Render Result":
                 continue
-            r = 2 ** max(0, ceil(log2(img.size[0] * img.size[1] / 2 ** 20)) // 2)
+            if self.to_small:
+                while img.size[0] > 1024 and img.size[1] > 1024:
+                    img.scale(img.size[0] // 2, img.size[1] // 2)
             img.filepath_raw = "/tmp/img/" + img.name.split(".")[0] + ".jpg"
-            if r > 1 and self.to_small:
-                img.scale(img.size[0] // r, img.size[1] // r)
-            img.save_render(img.filepath_raw, scene=bpy.context.scene)
+            with ChangeImageSettings(img, self.quality):
+                img.save_render(img.filepath_raw, scene=bpy.context.scene)
+            img.reload()
         bpy.ops.file.pack_all()
-        bpy.ops.image.save_all_modified()
+        # bpy.ops.image.save_all_modified()
+        bpy.context.scene.view_settings.view_transform = "Filmic"
         return {"FINISHED"}
 
 
